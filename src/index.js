885766ae821933d5980a9d228125e9f4fd73f5d2
Sylvester Kpei <kpeis695@gmail.com>
2025-06-07 02:18:00 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import SilverStore from '../SilverStore';
+import SilverStore from './SilverStore';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
